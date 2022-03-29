9be8c08edfbfa36729a0dd1f9907009b5b9c2a41
Rodrigo Della Justina <rodrigoj@sssistemas.com>
2022-03-28 21:26:22 -0300

    tratamento de erros com try catch

---
+++ teste-erros/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args){
+        metodo01();
+    }
+
+    public static void metodo01(){
+        System.out.println("Inicio :: metodo01");
+        metodo02();
+        System.out.println("Fim :: metodo01");
+    }
+
+    public static void metodo02(){
+        System.out.println("Inicio :: metodo02");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Posição do Array não existe");
+            e.printStackTrace();
+        }
+
+        System.out.println("Fim :: metodo02");
+
+
+    }
+
+
+
+}
