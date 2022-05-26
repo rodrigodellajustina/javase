ba09ffa0d0061f8fa98cff3469d70cc4ab005d09
Rodrigo Della Justina <rodrigoj@sssistemas.com>
2022-05-25 22:22:00 -0300

    capturando dados dos componentes de tela dentro do controller

--- javafx-fxml-01/src/Controller/LoginController.java
+++ javafx-fxml-01/src/Controller/LoginController.java
@@ -4,21 +4,32 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
     @FXML
     Button btLogar;
+    @FXML
     Button btCancelar;
+    @FXML
+    TextField tfUsuario = null;
+    @FXML
+    TextField tfSenha = null;
+    
 
     @FXML
     public void clickLogar(Event e){
-        Alert alertLogar = new Alert(Alert.AlertType.INFORMATION, "Clicou no botão logar");
+        String usuario = tfUsuario.getText();
+        String senha = tfSenha.getText();
+        Alert alertLogar = new Alert(Alert.AlertType.INFORMATION, "Clicou no botão logar " + usuario + " " + senha);
         alertLogar.show();
     }
 
     @FXML
     public void clickCancelar(Event e){
+        tfUsuario.setText("");
+        tfSenha.setText("");
         Alert alertCancelar = new Alert(Alert.AlertType.INFORMATION, "Clicou no botão cancelar");
         alertCancelar.show();
     }
